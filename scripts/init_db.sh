6e86476c316bbd88654c76dc1f8f1cc25bc5e8bd
root <root@LAPTOP-OGHAU7O7>
2024-03-03 19:55:08 +0000

    chore: update script

--- scripts/init_db.sh
+++ scripts/init_db.sh
@@ -1,7 +1,6 @@
 #!/usr/bin/env bash
 set -x
 set -eo pipefail
-
 if ! [ -x "$(command -v psql)" ]; then
   echo >&2 "Error: psql is not installed."
   exit 1
@@ -29,23 +28,31 @@ DB_HOST="${POSTGRES_HOST:=localhost}"
 DATABASE_URL=postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}:${DB_PORT}/${DB_NAME}
 export DATABASE_URL
 
-# Launch postgres using Docker
-docker run \
--e POSTGRES_USER=${DB_USER} \
--e POSTGRES_PASSWORD=${DB_PASSWORD} \
--e POSTGRES_DB=${DB_NAME} \
--p "${DB_PORT}":5432 \
--d postgres \
-postgres -N 1000
+# Allow to skip Docker if a dockerized Postgres database is already running
+if [[ -z "${SKIP_DOCKER}" ]]
+then
+  docker run \
+  -e POSTGRES_USER=${DB_USER} \
+  -e POSTGRES_PASSWORD=${DB_PASSWORD} \
+  -e POSTGRES_DB=${DB_NAME} \
+  -p "${DB_PORT}":5432 \
+  -d postgres \
+  postgres -N 1000
+fi
+
 # ^ Increased maximum number of connections for testing purposes
 
 # Keep pinging Postgres until it's ready to accept commands
 export PGPASSWORD="${DB_PASSWORD}"
 until psql -h "${DB_HOST}" -U "${DB_USER}" -p "${DB_PORT}" -d "postgres" -c '\q'; do
 >&2 echo "Postgres is still unavailable - sleeping"
-sleep 1
+  sleep 1
 done
+
 >&2 echo "Postgres is up and running on port ${DB_PORT}!"
+
 DATABASE_URL=postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}:${DB_PORT}/${DB_NAME}
 export DATABASE_URL
 sqlx database create
+sqlx migrate run
+>&2 echo "Postgres has been migrated, ready to go!"
