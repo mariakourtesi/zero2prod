cdf7b40451375222016236b9c9f2c2de96ec268c
root <root@LAPTOP-OGHAU7O7>
2024-03-06 20:27:23 +0000

    chore: modules

--- src/startup.rs
+++ src/startup.rs
@@ -1,6 +1,7 @@
 use actix_web::dev::Server;
-use actix_web::{ web, App, HttpResponse, HttpServer };
+use actix_web::{ web, App, HttpServer };
 use std::net::TcpListener;
+use crate::routes::{ health_check, subscribe, FormData };
 
 pub fn run(listener: TcpListener) -> Result<Server, std::io::Error> {
     let server = HttpServer::new(|| {
